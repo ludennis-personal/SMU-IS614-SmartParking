1a2f7588d273569f01b00d9d9b7f6583cc35fc2a
STRATPOINT-DennisLu <dennislu.personal@gmail.com>
2024-10-30 21:52:02 +0800

    Update Reference Point

--- js_version/reference_1.js
+++ js_version/reference_1.js
@@ -1,8 +1,9 @@
 radio.setGroup(1)
 radio.setTransmitPower(2)
-let sensor_id = "S1"
+let sensor_id = "s1"
 basic.forever(function () {
     basic.showString("S")
     radio.sendString(sensor_id)
+    serial.writeLine("" + (radio.receivedPacket(RadioPacketProperty.SignalStrength)))
     basic.pause(1000)
 })
